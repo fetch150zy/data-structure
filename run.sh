d741780a0d2b06c2e178d3a25666a633aa114c7c
fetch150zy <Mars_zhewei@outlook.com>
2022-09-25 13:57:11 +0800

    add script

--- run.sh
+++ run.sh
@@ -3,10 +3,21 @@
 cd build
 
 # for test list
+echo -e "\n***********************************************\n*"
+echo -e "*\n*              Test list                     *\n*"
+echo -e "*\n***********************************************\n"
 cmake .. -DUSE_LIST=ON && make && ./res; rm -rf *
 
+
 # for test link
+echo -e "\n***********************************************\n*"
+echo -e "*\n*              Test link                     *\n*"
+echo -e "*\n***********************************************\n"
 cmake .. -DUSE_LINK=ON && make && ./res; rm -rf *
 
+
 # for test static link
+echo -e "\n***********************************************\n*"
+echo -e "*\n*           Test static link                 *\n*"
+echo -e "*\n***********************************************\n"
 cmake .. -DUSE_STATIC_LINK=ON && make && ./res; rm -rf *
